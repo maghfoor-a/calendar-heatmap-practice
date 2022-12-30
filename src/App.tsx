3a87c3e1ecabb8b5e78ca44c4acb076d2d01f60a
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2022-12-30 22:11:12 +0000

    added small instructions

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import MainBody from "./MainBody";
 function App() {
   return (
     <>
-      <h1>Vite React TypeScript Template!</h1>
+      <h1>React Heatmap Practice</h1>
+      <h3>Select a date on the calendar and press the log button!</h3>
       <MainBody />
     </>
   );
